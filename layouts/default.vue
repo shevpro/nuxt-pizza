ce2a081886a0fc3bed70bba2e30b52d103164758
Dmitrii Shevchuk <shevchuk.pro@yandex.ru>
2025-03-13 19:05:14 +0200

    add nav component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import ClientHeader from '~/components/ClientHeader.vue';
+import ClientNav from '~/components/ClientNav.vue';
 </script>
 
 <template>
     <main>
-        <ClientHeader>header</ClientHeader>
+        <ClientHeader />
+        <ClientNav />
         <slot />
     </main>
 </template>
