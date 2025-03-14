4046e4505e4aa4940e8186e26180d0727ecd7ced
Dmitrii Shevchuk <shevchuk.pro@yandex.ru>
2025-03-14 19:50:26 +0200

    add basic showcase + productCard

---
+++ components/SectionShowcase.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Heading from '~/components/ui/Heading.vue';
+import ProductCard from '~/components/ui/ProductCard.vue';
+import type { IProduct } from '~/types/product';
+
+interface IProps {
+    title: string;
+    items: Partial<IProduct[] | null>;
+}
+
+defineProps<IProps>();
+</script>
+
+<template>
+    <section class="section-showcase container">
+        <Heading v-if="title" name="h2" class="section-showcase__title">{{ title }}</Heading>
+        <template v-for="item in items">
+            <ProductCard 
+                v-if="item"
+                :item="item"
+                :key="item.id"
+                class="section-showcase__item"
+            />
+        </template>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.section-showcase {
+    display: flex;
+    flex-wrap: wrap;
+
+    &__title {
+        width: 100%;
+        margin-bottom: 30px;
+    }
+
+    &__item {
+        margin-right: 37.3333px;
+        margin-bottom: 60px;
+
+        &:nth-child(4n+4) {
+            margin-right: 0;
+        }
+    }
+}
+</style>
---
+++ components/ui/ProductCard.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup>
+import type { IProduct } from '~/types/product';
+
+interface IProps {
+  item: IProduct;
+}
+
+defineProps<IProps>();
+</script>
+
+<template>
+  <article class="product-card">
+    <NuxtLink to="#" class="product-card__link"></NuxtLink>
+    <div class="product-card__body">
+      <picture class="product-card__picture">
+        <img :src="item.imgUrl" :alt="item.title">
+      </picture>
+      <span class="product-card__title">{{ item.title }}</span>
+      <p class="product-card__text">{{ item.caption }}</p>
+    </div>
+    <footer class="product-card__footer">
+      <div class="product-card__price">от {{ item.currentPrice }} ₽</div>
+      <div></div>
+    </footer>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.product-card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 292px;
+  justify-content: space-between;
+
+  &__link {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  &__picture {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    user-select: none;
+
+    img {
+      width: 100%;
+      object-fit: contain;
+    }
+  }
+
+  &__title {
+    display: block;
+    font-size: 20px;
+    line-height: 24px;
+    margin-bottom: 15px;
+  }
+
+  &__text {
+    color: rgb(92, 99, 112);
+    font-size: 14px;
+    line-height: 20px;
+    margin: 0;
+  }
+
+  &__footer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 25px;
+  }
+}
+</style>
