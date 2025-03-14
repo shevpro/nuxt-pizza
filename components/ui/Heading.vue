934c969cb4dd4b81670cc2466c4cf467efd7b59e
Dmitrii Shevchuk <shevchuk.pro@yandex.ru>
2025-03-14 19:49:38 +0200

    add heading component

---
+++ components/ui/Heading.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface IProps {
+    name: 'h1' | 'h2' | 'h3' | 'h4';
+}
+
+const props = defineProps<IProps>();
+
+const componentId = computed(() => `${props.name}`);
+const componentStyles = computed(() => ['heading', props.name]);
+</script>
+
+<template>
+    <component :is="componentId" :class="componentStyles"><slot /></component>
+</template>
+
+<style scoped lang="scss">
+.heading {
+    margin: 0;
+    padding: 0;
+}
+</style>
